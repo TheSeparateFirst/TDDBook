9cfb8cd6ed2ce618c2295a51b8c032e65763745a
Jeff Murray <jmurray@americanis.net>
2018-12-20 15:43:24 -0800

    Added some more tests for HTML response

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
 
+class NewVisitorTest(unittest.TestCase):
+
+    # Initialization and browser startup
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    # Cleanup
     def tearDown(self):
         self.browser.quit()
 
@@ -14,17 +20,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # He sees the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
 
         # He is invited to enter a to-do item straight away
-
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # He types "Purchase uptimewarriors.com" into a text box
+        inputbox.send_keys('Purchase uptimewarriors.com')
 
         # When He hits enter, the page updates, and now the page lists
 # "1: Purchase uptimewarriors.com" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Purchase uptimewarriors.com' for row in rows)
+        )
 
         # Thise is still a text box inviting him to add anothis item. He
         # enters "Make a wordpress blog for uptimewarriors.com"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on his list
 
